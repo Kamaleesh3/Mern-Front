d8891fdd0359ba5f48cf774f7cb43e22d4422b7a
Kamaleesh3 <161963087+Kamaleesh3@users.noreply.github.com>
2024-04-28 20:06:46 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ import { useEffect, useState} from "react";
 import store from './store'
 import { loadUser } from "./actions/userActions";
 import Profile from "./components/user/Profile";
-import ProtectedRoute from "./components/route/ProtuctedRoute";
 import UpdateProfile from "./components/user/UpdateProfile";
 import UpdatePassword from "./components/user/UpdatePassword";
 import ForgotPassword from "./components/user/ForgetPassword";
@@ -45,17 +44,18 @@ import Testimonial from "./components/pages/Testimonial";
 import Blog from "./components/pages/Blog";
 import Contact from "./components/pages/Contact";
 import Products from "./components/pages/Products";
+import About from "./components/pages/About";
 
 function App() {
-  // const [stripeApiKey, setStripeApiKey] = useState("")
-  // useEffect(()=>{
-  //   store.dispatch(loadUser)
-  //   async function getStripeApiKey(){
-  //     const {data} = await axios.get('https://mern-site.onrender.com/api/v1/stripeapi')
-  //     setStripeApiKey(data.stripeApiKey)
-  //   }
-  //   getStripeApiKey()
-  // },[])
+  const [stripeApiKey, setStripeApiKey] = useState("")
+  useEffect(()=>{
+    store.dispatch(loadUser)
+    async function getStripeApiKey(){
+      const {data} = await axios.get('http://localhost:8000/api/v1/stripeapi')
+      setStripeApiKey(data.stripeApiKey)
+    }
+    getStripeApiKey()
+  },[])
   return (
     <BrowserRouter>
       <div className="App">
@@ -64,45 +64,46 @@ function App() {
           <div>  
           <ToastContainer theme="dark"/>
           <Routes>
-            <Route path='/home' element={<Home />} />
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
             <Route path='/testimonial' element={<Testimonial />} />
             <Route path='/blog' element={<Blog />} />
             <Route path='/products' element={<Products/>} />
             <Route path='/contact' element={<Contact />} />
             <Route path='/search/:keyword' element={<ProductSearch />} />
             <Route path='/product/:id' element={<ProductDetail />} />
-            <Route path='/' element={<Login />} />
+            <Route path='/login' element={<Login />} />
             <Route path='/register' element={<Register />} />
-            <Route path='/myprofile' element={<ProtectedRoute><Profile /></ProtectedRoute>} />
-            <Route path='/myprofile/update' element={<ProtectedRoute><UpdateProfile /></ProtectedRoute>} />
-            <Route path='/myprofile/update/password' element={<ProtectedRoute><UpdatePassword /></ProtectedRoute>} />
+            <Route path='/myprofile' element={<Profile />} />
+            <Route path='/myprofile/update' element={<UpdateProfile />} />
+            <Route path='/myprofile/update/password' element={<UpdatePassword />} />
             <Route path='/password/forgot' element={<ForgotPassword/>} />
             <Route path='/password/reset/:token' element={<ResetPassword/>} />
             <Route path='/cart' element={<Cart/>} />
-            <Route path='/shipping' element={<ProtectedRoute><Shipping /></ProtectedRoute>} />
-            <Route path='/order/confirm' element={<ProtectedRoute><ConfirmOrder /></ProtectedRoute>} />
-            <Route path='/order/success' element={<ProtectedRoute><OrderSuccess /></ProtectedRoute>} />
-            <Route path='/orders' element={<ProtectedRoute><UserOrders /></ProtectedRoute>} />
-            <Route path='/order/:id' element={<ProtectedRoute><OrderDetails /></ProtectedRoute>} />
+            <Route path='/shipping' element={<Shipping />} />
+            <Route path='/order/confirm' element={<ConfirmOrder />} />
+            <Route path='/order/success' element={<OrderSuccess />} />
+            <Route path='/orders' element={<UserOrders />} />
+            <Route path='/order/:id' element={<OrderDetails />} />
            
-            {/* {stripeApiKey && <Route path='/payment' element={<ProtectedRoute><Elements stripe={loadStripe(stripeApiKey)}><Payment/></Elements></ProtectedRoute> } />
-}  */}
+             {stripeApiKey && <Route path='/payment' element={<Elements stripe={loadStripe(stripeApiKey)}><Payment/></Elements> } />
+}  
             </Routes>
           </div>
           <Routes>
 
           <Route path='/admin' element={<AdminLogin />} />
-          <Route path='/admin/dashboard' element={<ProtectedRoute isAdmin={true}><Dashboard /></ProtectedRoute>} />
+          <Route path='/admin/dashboard' element={<Dashboard />} />
           <Route path='/admin/categerious' element={ <Categerious/> } />
           <Route path='/admin/category' element={ <Category/> } />
-          <Route path='/admin/products' element={<ProtectedRoute isAdmin={true}><ProductList /></ProtectedRoute>} />
-          <Route path='/admin/products/create' element={<ProtectedRoute isAdmin={true}><NewProduct /></ProtectedRoute>} />
-          <Route path='/admin/product/:id' element={<ProtectedRoute isAdmin={true}><UpdateProduct /></ProtectedRoute>} />
-          <Route path='/admin/orders' element={ <ProtectedRoute isAdmin={true}><OrderList/></ProtectedRoute> } />
-          <Route path='/admin/order/:id' element={ <ProtectedRoute isAdmin={true}><UpdateOrder/></ProtectedRoute> } />
-          <Route path='/admin/users' element={ <ProtectedRoute isAdmin={true}><UserList/></ProtectedRoute> } />
-          <Route path='/admin/user/:id' element={ <ProtectedRoute isAdmin={true}><UpdateUser/></ProtectedRoute> } />
-          <Route path='/admin/reviews' element={ <ProtectedRoute isAdmin={true}><ReviewList/></ProtectedRoute> } />
+          <Route path='/admin/products' element={<ProductList />} />
+          <Route path='/admin/products/create' element={<NewProduct />} />
+          <Route path='/admin/product/:id' element={<UpdateProduct />} />
+          <Route path='/admin/orders' element={ <OrderList/> } />
+          <Route path='/admin/order/:id' element={ <UpdateOrder/> } />
+          <Route path='/admin/users' element={ <UserList/> } />
+          <Route path='/admin/user/:id' element={ <UpdateUser/> } />
+          <Route path='/admin/reviews' element={ <ReviewList/> } />
           </Routes>
           <Footer />
         </HelmetProvider>
